c44d17379cf50786a7cfa69ee1422568ce1e7031
Chameleon Cloud User <cc@hw4-team11-skylake-tn.novalocal>
2020-04-09 17:23:48 +0000

    plot.py finalized

--- plot.py
+++ plot.py
@@ -16,24 +16,25 @@ def read_columns(filename):
 def get_line(filename, column):
     with open(filename) as file:
         for line in file:
-            # print(line.split())
-            if line[0] == '0':
-                # print(line.split()[1:])
+         #   print(line[0])
+            if line[0] == '1':
+          #      print(line.split()[1:])
                 yield line.split(maxsplit=column-1)
 
 
 if __name__ == "__main__":
 
-        filename = "data"
+        filename = "64GB_data"
         columns = read_columns(filename)
-
-        columns = columns[:]
+        columns = columns[:] 
+    
+    
 
         df = pd.DataFrame.from_records(get_line(filename, len(columns)), columns=columns)
 
         df = df.drop(columns=["Command", "iodelay", "Time", "UID", "%usr", "%guest", "%system",
                               "majflt/s", "%wait", "CPU", "minflt/s", "RSS", "VSZ", "kB_ccwr/s"])
-        print(df["%MEM"])
+        #print(df)
 
 
         df['%CPU'] = df['%CPU'].astype(str).astype(float)
@@ -42,31 +43,54 @@ if __name__ == "__main__":
         df["kB_wr/s"] = df['kB_wr/s'].astype(str).astype(float)
 
         df["%MEM"] = df["%MEM"]*1.87
+        #print(df["%CPU"])
 
-        t = range(0,183)
         # data1 = np.exp(t)
         data1 = df["%CPU"][:-1]
         # data2 = np.sin(2 * np.pi * t)
         data2 = df["%MEM"][:-1]
-        print(data2)
+        #print(data2)
         # data3 = np.cos(2 * np.pi * t)
         data3 = df["kB_rd/s"]/1000000 + df["kB_wr/s"]/1000000
         data3 = data3[:-1]
-        print(data3)
+        #print(data3)
 
+        t = range(0,len(data3))
         fig, ax1 = plt.subplots()
+
         color = 'tab:red'
         ax1.set_xlabel('time (s)')
         ax1.set_ylabel('CPU (%)', color=color)
-        ax1.plot(t, data1, color=color)
+        ax1.plot(t, data1, color=color, label="CPU")
         ax1.tick_params(axis='y', labelcolor=color)
-        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-        color = 'tab:blue'
-        ax2.set_ylabel('Memory', color=color)  # we already handled the x-label with ax1
-        ax2.plot(t, data2, color=color)
+        plt.xticks(np.arange(0, 950, 50))
+        ax1.legend(bbox_to_anchor=(0.062,1.006))
 
-        ax2.plot(t, data3, color='tab:orange')
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+        color = 'tab:blue'
+        ax2.set_ylabel('Memory (GB)', color=color)  # we already handled the x-label with ax1
+        ax2.plot(t, data2, color=color, label="MEM")
+        ax2.plot(t, data3, color='tab:orange', label="IO")
         ax2.tick_params(axis='y', labelcolor=color)
+        
+        plt.title("64GB - CPU, MEMORY, I/O")
+        plt.yticks(np.arange(0, 10.5, 0.5))
+        plt.legend()
+
 
         fig.tight_layout()  # otherwise the right y-label is slightly clipped
         plt.show()
+
+
+
+        data3 = df["kB_rd/s"] / 1000 + df["kB_wr/s"] / 1000
+        data3 = data3[:-1]
+        plt.title("I/O utilization MB/sec")
+        plt.xlabel("Time (seconds)")
+        plt.ylabel("IO (MB/sec)")
+        plt.xticks(np.arange(0,len(data3),50))
+        plt.plot(t, data3, label="I/O",)
+        plt.legend()
+        plt.show()
+
